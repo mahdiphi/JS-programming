9910f1b67366a1acd416382400ef4474bad315f6
Mahdi`PHi <121295421+mahdiphi@users.noreply.github.com>
2025-03-15 20:03:46 +0330

    one

---
+++ 1. number/script.js
@@ -0,0 +1,17 @@
+// const convert = () => {
+
+//     var value = document.getElementById("dama").value;
+//     var far = document.getElementById("dam").value;
+
+//     console.log(far)
+
+//     var result;
+
+//     if(far == on){
+//          return (result = ( value - 32) / 1.8)
+//     }else{
+//         console.log("Error")
+//     }
+// }
+
+// console.log(result)
